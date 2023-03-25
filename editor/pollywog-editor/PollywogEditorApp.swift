c701dd86308e08678847a43b7e6f69a75c28a91d
Michael Biggs <michael@biggs.io>
2023-03-25 10:13:06 -0400

    Added Starter SwiftUI macOS Editor Project

---
+++ editor/pollywog-editor/PollywogEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PollywogEditorApp.swift
+//  pollywog-editor
+//
+//  Created by Michael Biggs on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PollywogEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
